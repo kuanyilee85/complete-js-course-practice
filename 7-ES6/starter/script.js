2890cd3b06044c9a2005a751a835258ef12a537f
kuanyilee85 <kuanyilee85@gmail.com>
2019-10-26 22:35:18 +0800

    The spread oprator

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -203,17 +203,17 @@ console.log(age2);
 console.log(retirement);
 
 // Lecture: Arrays; For of,
-const boxes = document.querySelectorAll(".box");
+// const boxes = document.querySelectorAll(".box");
 // ES5
 
-var boxesArr5 = Array.prototype.slice.call(boxes);
-boxesArr5.forEach(function(cur) {
-  cur.style.backgroundColor = "dodgerblue";
-});
+// var boxesArr5 = Array.prototype.slice.call(boxes);
+// boxesArr5.forEach(function(cur) {
+//   cur.style.backgroundColor = "dodgerblue";
+// });
 
 // ES6
-const boxesArr6 = Array.from(boxes);
-Array.from(boxes).forEach(cur => (cur.style.backgroundColor = "dodgerblue"));
+// const boxesArr6 = Array.from(boxes);
+// Array.from(boxes).forEach(cur => (cur.style.backgroundColor = "dodgerblue"));
 
 // ES5
 // for (var j = 0; j < boxesArr5.length; j++) {
@@ -225,25 +225,55 @@ Array.from(boxes).forEach(cur => (cur.style.backgroundColor = "dodgerblue"));
 // }
 
 // ES6
-for (const cur of boxesArr6) {
-  if (cur.className.includes("blue")) {
-    continue;
-  } else {
-    cur.textContent = "I changed to blue.";
-  }
-}
+// for (const cur of boxesArr6) {
+//   if (cur.className.includes("blue")) {
+//     continue;
+//   } else {
+//     cur.textContent = "I changed to blue.";
+//   }
+// }
 
 //ES5, index of
-var ages = [12, 13, 15, 17, 11, 14];
+// var ages = [12, 13, 15, 17, 11, 14];
 
-var full = ages.map(function(cur) {
-  return cur >= 16;
-});
+// var full = ages.map(function(cur) {
+//   return cur >= 16;
+// });
 
-console.log(full);
-console.log(full.indexOf(true));
-console.log(ages[full.indexOf(true)]);
+// console.log(full);
+// console.log(full.indexOf(true));
+// console.log(ages[full.indexOf(true)]);
 
 // ES6
-console.log(ages.findIndex(cur => cur >= 16));
-console.log(ages.find(cur => cur >= 16));
+// console.log(ages.findIndex(cur => cur >= 16));
+// console.log(ages.find(cur => cur >= 16));
+
+// Lecture: Spread operator
+//convenient way to expand elements of an array in place of like argument and function calls
+function addFourAges(a, b, c, d) {
+  return a + b + c + d;
+}
+
+var sum1 = addFourAges(18, 30, 12, 21);
+console.log(sum1);
+
+//ES5
+var ages = [18, 30, 12, 21];
+var sum2 = addFourAges.apply(null, ages);
+console.log(sum2);
+
+//ES6
+const sum3 = addFourAges(...ages);
+console.log(sum3);
+
+const familySmith = ["John", "Jane", "Mark"];
+const familyMiller = ["Mary", "Bob", "Ann"];
+const bigFamily = [...familySmith, "Lily", ...familyMiller];
+console.log(bigFamily);
+
+const h = document.querySelector("h1");
+const boxes = document.querySelectorAll(".box");
+const all = [h, ...boxes];
+
+//transfer from nodeList to array by from() and loop through bu forEach()
+Array.from(all).forEach(cur => (cur.style.color = "purple"));
