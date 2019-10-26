c93f87e91574a5eb708196ff5fc32d84cfcdf48c
kuanyilee85 <kuanyilee85@gmail.com>
2019-10-26 22:19:16 +0800

    Arrays

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -201,3 +201,49 @@ const [age2, retirement] = calcAgeRetirement(1990);
 
 console.log(age2);
 console.log(retirement);
+
+// Lecture: Arrays; For of,
+const boxes = document.querySelectorAll(".box");
+// ES5
+
+var boxesArr5 = Array.prototype.slice.call(boxes);
+boxesArr5.forEach(function(cur) {
+  cur.style.backgroundColor = "dodgerblue";
+});
+
+// ES6
+const boxesArr6 = Array.from(boxes);
+Array.from(boxes).forEach(cur => (cur.style.backgroundColor = "dodgerblue"));
+
+// ES5
+// for (var j = 0; j < boxesArr5.length; j++) {
+//   if (boxesArr5[j].className === "box blue") {
+//     continue;
+//   } else {
+//     boxesArr5[j].textContent = "I changed to blue.";
+//   }
+// }
+
+// ES6
+for (const cur of boxesArr6) {
+  if (cur.className.includes("blue")) {
+    continue;
+  } else {
+    cur.textContent = "I changed to blue.";
+  }
+}
+
+//ES5, index of
+var ages = [12, 13, 15, 17, 11, 14];
+
+var full = ages.map(function(cur) {
+  return cur >= 16;
+});
+
+console.log(full);
+console.log(full.indexOf(true));
+console.log(ages[full.indexOf(true)]);
+
+// ES6
+console.log(ages.findIndex(cur => cur >= 16));
+console.log(ages.find(cur => cur >= 16));
