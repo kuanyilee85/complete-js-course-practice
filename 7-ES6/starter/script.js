7501c1bc373a45499eb8b96462790860a852e092
kuanyilee85 <kuanyilee85@gmail.com>
2019-10-26 17:35:08 +0800

    arrow function2 and destructuring

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -30,39 +30,39 @@ console.log("shows not defined: ");
 
 //Lecture: strings
 
-let firstName = "John";
-let lastName = "Smith";
-const yearOfBirth = 1990;
-function calcAge(year) {
-  return 2019 - year;
-}
+// let firstName = "John";
+// let lastName = "Smith";
+// const yearOfBirth = 1990;
+// function calcAge(year) {
+//   return 2019 - year;
+// }
 
 // ES5
-console.log(
-  "This is " +
-    firstName +
-    " " +
-    lastName +
-    ". He was born in " +
-    yearOfBirth +
-    ". Today, he is " +
-    calcAge(yearOfBirth) +
-    " years old."
-);
+// console.log(
+//   "This is " +
+//     firstName +
+//     " " +
+//     lastName +
+//     ". He was born in " +
+//     yearOfBirth +
+//     ". Today, he is " +
+//     calcAge(yearOfBirth) +
+//     " years old."
+// );
 
 // ES6 use BACKTICK ``
-console.log(
-  `This is ${firstName} ${lastName}. He was born in ${yearOfBirth}. Today, he is ${calcAge(
-    yearOfBirth
-  )} years old.`
-);
+// console.log(
+//   `This is ${firstName} ${lastName}. He was born in ${yearOfBirth}. Today, he is ${calcAge(
+//     yearOfBirth
+//   )} years old.`
+// );
 
-const n = `${firstName} ${lastName}`;
+// const n = `${firstName} ${lastName}`;
 
-console.log(n.startsWith("J"));
-console.log(n.endsWith("th"));
-console.log(n.includes("???"));
-console.log(`${firstName} `.repeat(5));
+// console.log(n.startsWith("J"));
+// console.log(n.endsWith("th"));
+// console.log(n.includes("???"));
+// console.log(`${firstName} `.repeat(5));
 
 // Lecture: Arrow Functions
 
@@ -93,3 +93,111 @@ ages6 = years.map((el, index) => {
 });
 
 console.log(ages6);
+
+// Lecture: arrow function 2
+// ES5
+var box5 = {
+  color: "green",
+  position: 1,
+  clickMe: function() {
+    var self = this;
+    document.querySelector(".green").addEventListener("click", function() {
+      var str =
+        "This is box number " + self.position + " and it is " + self.color;
+      alert(str);
+    });
+  }
+};
+// box5.clickMe();
+
+// ES6
+var box6 = {
+  color: "green",
+  position: 1,
+  clickMe: function() {
+    document.querySelector(".green").addEventListener("click", () => {
+      var str =
+        "This is box number " + this.position + " and it is " + this.color;
+      alert(str);
+    });
+  }
+};
+// box6.clickMe();
+
+// ES6
+
+// 'this' key word is shared by its surrounding content
+// var box66 = {
+//   color: "green",
+//   position: 1,
+//   clickMe: () => {
+//     document.querySelector(".green").addEventListener("click", () => {
+//       var str =
+//         "This is box number " + this.position + " and it is " + this.color;
+//       alert(str);
+//     });
+//   }
+// };
+// box66.clickMe();
+
+function Person(name) {
+  this.name = name;
+}
+
+// ES5
+// Person.prototype.myFriends5 = function(friends) {
+//   var arr = friends.map(
+//     function(el) {
+//       return this.name + " is friends with " + el;
+//     }.bind(this)
+//   );
+//   console.log(arr);
+// };
+
+// var friends = ["bob", "jane", "john"];
+// new Person("mark").myFriends5(friends);
+
+// ES6
+
+Person.prototype.myFriends6 = function(friends) {
+  var arr = friends.map(el => `${this.name} is friends with ${el}`);
+  console.log(arr);
+};
+
+var friends = ["bob", "jane", "john"];
+new Person("Dave").myFriends6(friends);
+
+// Lecture: Destructuring
+
+//ES5
+var john = ["john", 26];
+// var name = john[0];
+// var age = john[1];
+
+// ES6
+const [name, age] = ["Mark", "30"];
+console.log(name);
+console.log(age);
+
+const obj = {
+  firstName: "John",
+  lastName: "Smith"
+};
+
+const { firstName, lastName } = obj;
+console.log(firstName);
+console.log(lastName);
+
+const { firstName: a, lastName: b } = obj;
+console.log(a);
+console.log(b);
+
+function calcAgeRetirement(year) {
+  const age2 = new Date().getFullYear() - year;
+  return [age2, 65 - age2];
+}
+
+const [age2, retirement] = calcAgeRetirement(1990);
+
+console.log(age2);
+console.log(retirement);
