cf91f01fe7c69d476f9029314862d996ccd1bf83
kuanyilee85 <kuanyilee85@gmail.com>
2019-10-27 11:15:41 +0800

    Default parameter, Map() for harsh table

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -1,4 +1,6 @@
 // Lecture: variables
+
+/*
 let i = 23;
 
 console.log(
@@ -28,44 +30,49 @@ console.log("shows not defined: ");
 
 // console.log("shows not defined: " + a + b);
 
+*/
+
 //Lecture: strings
 
-// let firstName = "John";
-// let lastName = "Smith";
-// const yearOfBirth = 1990;
-// function calcAge(year) {
-//   return 2019 - year;
-// }
+/*
+let firstName = "John";
+let lastName = "Smith";
+const yearOfBirth = 1990;
+function calcAge(year) {
+  return 2019 - year;
+}
 
-// ES5
-// console.log(
-//   "This is " +
-//     firstName +
-//     " " +
-//     lastName +
-//     ". He was born in " +
-//     yearOfBirth +
-//     ". Today, he is " +
-//     calcAge(yearOfBirth) +
-//     " years old."
-// );
+ES5
+console.log(
+  "This is " +
+    firstName +
+    " " +
+    lastName +
+    ". He was born in " +
+    yearOfBirth +
+    ". Today, he is " +
+    calcAge(yearOfBirth) +
+    " years old."
+);
 
-// ES6 use BACKTICK ``
-// console.log(
-//   `This is ${firstName} ${lastName}. He was born in ${yearOfBirth}. Today, he is ${calcAge(
-//     yearOfBirth
-//   )} years old.`
-// );
+ES6 use BACKTICK ``
+console.log(
+  `This is ${firstName} ${lastName}. He was born in ${yearOfBirth}. Today, he is ${calcAge(
+    yearOfBirth
+  )} years old.`
+);
 
-// const n = `${firstName} ${lastName}`;
+const n = `${firstName} ${lastName}`;
 
-// console.log(n.startsWith("J"));
-// console.log(n.endsWith("th"));
-// console.log(n.includes("???"));
-// console.log(`${firstName} `.repeat(5));
+console.log(n.startsWith("J"));
+console.log(n.endsWith("th"));
+console.log(n.includes("???"));
+console.log(`${firstName} `.repeat(5));
+
+*/
 
 // Lecture: Arrow Functions
-
+/*
 const years = [1902, 1932, 1982, 1950, 1999];
 
 //ES5
@@ -94,7 +101,11 @@ ages6 = years.map((el, index) => {
 
 console.log(ages6);
 
+*/
+
 // Lecture: arrow function 2
+
+/*
 // ES5
 var box5 = {
   color: "green",
@@ -126,36 +137,36 @@ var box6 = {
 
 // ES6
 
-// 'this' key word is shared by its surrounding content
-// var box66 = {
-//   color: "green",
-//   position: 1,
-//   clickMe: () => {
-//     document.querySelector(".green").addEventListener("click", () => {
-//       var str =
-//         "This is box number " + this.position + " and it is " + this.color;
-//       alert(str);
-//     });
-//   }
-// };
-// box66.clickMe();
+'this' key word is shared by its surrounding content
+var box66 = {
+  color: "green",
+  position: 1,
+  clickMe: () => {
+    document.querySelector(".green").addEventListener("click", () => {
+      var str =
+        "This is box number " + this.position + " and it is " + this.color;
+      alert(str);
+    });
+  }
+};
+box66.clickMe();
 
 function Person(name) {
   this.name = name;
 }
 
 // ES5
-// Person.prototype.myFriends5 = function(friends) {
-//   var arr = friends.map(
-//     function(el) {
-//       return this.name + " is friends with " + el;
-//     }.bind(this)
-//   );
-//   console.log(arr);
-// };
+Person.prototype.myFriends5 = function(friends) {
+  var arr = friends.map(
+    function(el) {
+      return this.name + " is friends with " + el;
+    }.bind(this)
+  );
+  console.log(arr);
+};
 
-// var friends = ["bob", "jane", "john"];
-// new Person("mark").myFriends5(friends);
+var friends = ["bob", "jane", "john"];
+new Person("mark").myFriends5(friends);
 
 // ES6
 
@@ -201,6 +212,8 @@ const [age2, retirement] = calcAgeRetirement(1990);
 
 console.log(age2);
 console.log(retirement);
+
+*/
 
 // Lecture: Arrays; For of,
 // const boxes = document.querySelectorAll(".box");
@@ -248,8 +261,9 @@ console.log(retirement);
 // console.log(ages.findIndex(cur => cur >= 16));
 // console.log(ages.find(cur => cur >= 16));
 
-// Lecture: Spread operator
+//**********************************************Lecture: Spread operator */
 //convenient way to expand elements of an array in place of like argument and function calls
+/*
 function addFourAges(a, b, c, d) {
   return a + b + c + d;
 }
@@ -277,3 +291,131 @@ const all = [h, ...boxes];
 
 //transfer from nodeList to array by from() and loop through bu forEach()
 Array.from(all).forEach(cur => (cur.style.color = "purple"));
+
+*/
+
+//*****************************************Rest parameter */
+//aloow us to pass an arbitrary number of argument into a function
+
+/*
+function isFullAge5() {
+  //console.log(arguments);
+  //transfer argument object into array
+  var argsArr = Array.prototype.slice.call(arguments);
+
+  //cur means the current elements of array from the argument
+  argsArr.forEach(function(cur) {
+    console.log(2016 - cur >= 18);
+  });
+}
+isFullAge5(1990, 1999, 1965);
+
+// ES6
+//APREAD OPERATOR IS USED IN FUNCTION CALL, WHILE REST PARAMETER IS USED IN FUNCTION DECLARATION TO SET ARBITRARY NUMBER
+function isFullAge6(...years) {
+  console.log(years);
+  years.forEach(cur => console.log(2016 - cur >= 18));
+}
+isFullAge6(1990, 1999, 1965, 2016, 1987);
+*/
+
+//ES5
+
+/*
+function isFullAge5(limit) {
+  var argsArr = Array.prototype.slice.call(arguments, 1);
+
+  argsArr.forEach(function(cur) {
+    console.log(2016 - cur >= limit);
+  });
+}
+
+//isFullAge5(16, 1990, 1999, 1965);
+isFullAge5(1990, 1999, 1965, 2016, 1987);
+
+//ES6
+function isFullAge6(limit, ...years) {
+  years.forEach(cur => console.log(2016 - cur >= limit));
+}
+
+isFullAge6(16, 1990, 1999, 1965, 2016, 1987);
+
+*/
+
+/****************Lecture: Default parameters*/
+//when you want the function have default value, preset
+
+//ES5
+
+/*
+function SmithPerson(firstName, yearOfBirth, lastName, nationality) {
+  lastName === undefined ? (lastName = "Smith") : (lastName = lastName);
+  nationality === undefined
+    ? (nationality = "american")
+    : (nationality = nationality);
+
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.yearOfBirth = yearOfBirth;
+  this.nationality = nationality;
+}
+var john = new SmithPerson("John", 1990);
+var emily = new SmithPerson("Emily", 1983, "Diaz", "spanish");
+*/
+
+//ES6
+/*
+function SmithPerson(
+  firstName,
+  yearOfBirth,
+  lastName = "Smith",
+  nationality = "american"
+) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.yearOfBirth = yearOfBirth;
+  this.nationality = nationality;
+}
+var john = new SmithPerson("John", 1990);
+var emily = new SmithPerson("Emily", 1983, "Diaz", "spanish");
+*/
+
+/******************Lesture: MAPS **************************************/
+//Harsh table: map() string keys to arbitrary values in ES6
+/*
+const question = new Map();
+question.set(
+  "question",
+  "what is the offical name of the lastest major JavaScript version?"
+);
+question.set(1, "ES5");
+question.set(2, "ES6");
+question.set(3, "ES2015");
+question.set(4, "ES7");
+question.set("correct", 3);
+question.set(true, "Correct number:D");
+question.set(false, "wrong, please try again!");
+console.log(question.get("question"));
+console.log(question.size);
+
+// if (question.has(4)) {
+//   question.delete(4);
+//   console.log("Answer 4 is here");
+// }
+
+//question.clear();
+
+//question.forEach((value, key) => console.log(`This is ${key}, and it's set to ${value}`));
+
+for (let [key, value] of question.entries()) {
+  if (typeof key === "number") {
+    console.log(`Answer ${key}: ${value}`);
+  }
+}
+
+const ans = parseInt(prompt("Write the correct answer"));
+console.log(question.get(ans === question.get("correct")));
+
+*/
+
+/**********************Lecture *****************************/
